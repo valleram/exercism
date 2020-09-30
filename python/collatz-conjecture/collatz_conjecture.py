1598be33e8ade4a4b453fdc03016c98ea75d2fe0
valleram <cuitlahuac92@hotmail.com>
2020-09-30 14:13:04 -0500

    Collatz conjecture code added

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,9 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Should be a positive integer")
+    else:
+        step = 0
+        while number > 1:
+            number = number/2 if number % 2 == 0 else (3*number) + 1
+            step += 1
+        return step
