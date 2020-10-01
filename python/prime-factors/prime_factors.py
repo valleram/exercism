4eab3b08b43429fec89134bf3d9055cfd81ad7de
valleram <cuitlahuac92@hotmail.com>
2020-10-01 12:54:08 -0500

    added prime_factors.py code

---
+++ python/prime-factors/prime_factors.py
@@ -0,0 +1,11 @@
+def factors(value):
+    prime_factors = []
+    factor = 2
+    while value > 1:
+        if value % factor == 0:
+            prime_factors.append(factor)
+            value = value / factor
+        else:
+            factor += 1
+    return prime_factors
+
