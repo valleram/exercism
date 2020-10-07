a1bf045cb61f73c5262bf2f2cd54d03289e6fbf7
valleram <cuitlahuac92@hotmail.com>
2020-10-07 10:13:11 -0500

    Added encode logic with provided keys

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,4 +1,5 @@
 import string
+import math
 
 class Cipher:
     def __init__(self, key=None):
@@ -6,12 +7,18 @@ class Cipher:
 
     def encode(self, text):
         alphabet = string.ascii_lowercase
-        data = []
-        for i in text.lower():
-            if i in alphabet:
-                data.append(alphabet[(alphabet.index(i) + 3) % 26])
-        output = ''.join(data)
-        return output
+        if self.key:
+            if len(self.key) < len(text):
+                lens = math.ceil((len(text) // len(self.key)) + 1)
+                temp = self.key * lens
+                self.key = temp[0:len(text)]
+            do = [alphabet.index(i) for i in self.key]
+            el = [alphabet[(alphabet.index(text[j]) + do[j]) % 26] for j in range(len(text))]
+            return "".join(el)
+        else:
+            data = [alphabet[(alphabet.index(i) + 3) % 26] for i in text if i in alphabet]
+            return "".join(data)
+
 
     def decode(self, text):
         alphabet = string.ascii_lowercase
