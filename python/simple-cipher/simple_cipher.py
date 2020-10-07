26fef9c38f6d575afb6ea62a997845ae68ac2581
valleram <cuitlahuac92@hotmail.com>
2020-10-07 17:01:05 -0500

    Finished simple_cipher.py

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,30 +1,38 @@
 import string
 import math
+from secrets import choice
+
 
 class Cipher:
+    ALPHABET = string.ascii_lowercase
     def __init__(self, key=None):
-        self.key = key
+        if key:
+            self.key = key
+        else:
+            self.key = "".join(choice(string.ascii_lowercase) for l in range(40))
 
-    def encode(self, text):
-        alphabet = string.ascii_lowercase
+    def get_key(self, text):
         if self.key:
             if len(self.key) < len(text):
                 lens = math.ceil((len(text) // len(self.key)) + 1)
                 temp = self.key * lens
                 self.key = temp[0:len(text)]
-            do = [alphabet.index(i) for i in self.key]
-            el = [alphabet[(alphabet.index(text[j]) + do[j]) % 26] for j in range(len(text))]
-            return "".join(el)
-        else:
-            data = [alphabet[(alphabet.index(i) + 3) % 26] for i in text if i in alphabet]
-            return "".join(data)
+            else:
+                self.key = self.key[0:len(text)]
+        return self.key
 
+    def encode(self, text):
+        self.key = self.get_key(text)
+        if self.key:
+            index_list = [Cipher.ALPHABET.index(i) for i in self.key]
+            return "".join([Cipher.ALPHABET[(Cipher.ALPHABET.index(text[j]) + index_list[j]) % 26] for j in range(len(text))])
+        else:
+            return "".join([Cipher.ALPHABET[(Cipher.ALPHABET.index(i) + 3) % 26] for i in text if i in Cipher.ALPHABET])
 
     def decode(self, text):
-        alphabet = string.ascii_lowercase
-        data = []
-        for i in text.lower():
-            if i in alphabet:
-                data.append(alphabet[(alphabet.index(i) - 3) % 26])
-        output = ''.join(data)
-        return output
+        self.key = self.get_key(text)
+        if self.key:
+            index_list = [Cipher.ALPHABET.index(i) for i in self.key]
+            return "".join([Cipher.ALPHABET[(Cipher.ALPHABET.index(text[j]) - index_list[j]) % 26] for j in range(len(text))])
+        else:
+            return "".join([Cipher.ALPHABET[(Cipher.ALPHABET.index(i) + 3) % 26] for i in text if i in Cipher.ALPHABET])
