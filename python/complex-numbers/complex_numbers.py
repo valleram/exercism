e2793748c34ed316e3d15afdb3744bcada8c04f0
valleram <cuitlahuac92@hotmail.com>
2020-10-12 18:53:40 -0500

    Added encode logic with provided keys

--- python/complex-numbers/complex_numbers.py
+++ python/complex-numbers/complex_numbers.py
@@ -1,27 +1,29 @@
+
 class ComplexNumber:
     def __init__(self, real, imaginary):
-        pass
+        self.real = real
+        self.imaginary = imaginary
 
     def __eq__(self, other):
-        pass
+        return self.real == other.real and self.imaginary == other.imaginary
 
     def __add__(self, other):
-        pass
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
 
     def __mul__(self, other):
-        pass
+        return ComplexNumber(self.real * other.real, self.imaginary * other.imaginary)
 
     def __sub__(self, other):
-        pass
+        return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
 
     def __truediv__(self, other):
+        #return ComplexNumber((self.real + self.imaginary / other.real + other.imaginary) * (other.real + (other.imaginary * -1) / other.real + (other.imaginary * -1)))
         pass
-
     def __abs__(self):
         pass
 
     def conjugate(self):
-        pass
+        return ComplexNumber(self.real, self.imaginary * -1)
 
     def exp(self):
         pass
--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,10 +1,17 @@
 def triplets_with_sum(number):
-    pass
-
-
-def triplets_in_range(start, end):
-    pass
+    triplets_list = list()
+    for a in range(1, number // 2):
+        for b in range(a + 1, number):
+            c = number - a - b
+            triplet = [a, b, c]
+            if is_triplet(triplet):
+                triplets_list.append(triplet)
+    return triplets_list
 
 
 def is_triplet(triplet):
-    pass
+    [a, b, c] = triplet
+    if a < b and b < c:
+        return a ** 2 + b ** 2 == c ** 2
+    else:
+        return False
