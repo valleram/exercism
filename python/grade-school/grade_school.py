d5e52caa855ed3173f4ac2fab39b2fec55c202f7
valleram <cuitlahuac92@hotmail.com>
2020-10-23 12:17:40 -0500

    code added to grade_school.py

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,22 @@
 class School:
     def __init__(self):
-        pass
+        self.students = {}
 
     def add_student(self, name, grade):
-        pass
+        if not grade in self.students:
+            self.students[grade] = [name]
+        else:
+            self.students[grade].append(name)
 
     def roster(self):
-        pass
+        student_list = []
+        sorted_grades = sorted(self.students.keys())
+        for i in sorted_grades:
+            get_students = self.students[i]
+            student_list.extend(sorted(get_students))
+        return student_list
 
     def grade(self, grade_number):
-        pass
+        if self.students:
+            return sorted(self.students[grade_number])
+        return []
