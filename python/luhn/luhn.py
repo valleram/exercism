f1a5167284416e9356d0d78bc0cf96705d101638
valleram <cuitlahuac92@hotmail.com>
2020-10-30 11:11:38 -0600

    Added code

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -3,4 +3,16 @@ class Luhn:
         self.card_num = card_num.replace(" ", "")
 
     def valid(self):
-        pass
+        card_num_lst = list(self.card_num)
+        if len(card_num_lst) <= 1:
+            return False
+        for i in range(len(card_num_lst)):
+            if card_num_lst[i].isdigit():
+                if i % 2 == 0:
+                    card_num_lst[i] = int(card_num_lst[i]) * 2
+                    if card_num_lst[i] > 9:
+                        card_num_lst[i] = int(card_num_lst[i]) - 9
+                else:
+                    card_num_lst[i] = int(card_num_lst[i])
+            else: return False
+        return sum(card_num_lst) % 10 == 0
